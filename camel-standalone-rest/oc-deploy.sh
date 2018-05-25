727524087be817c40e2d87b2dccf296d9615a93f
Venu Surampudi <venusurampudi@Venus-MBP-2.home>
2018-05-25 10:47:02 -0500

    added docker strategy

--- camel-standalone-rest/oc-deploy.sh
+++ camel-standalone-rest/oc-deploy.sh
@@ -1,10 +1,10 @@
 oc login -u=admin -p=admin
 oc project my-project
 oc delete all --all
-oc create configmap filebeat-url --from-file=./config.json
-oc new-app svsvenu/fabric-camel-rest:1.0-SNAPSHOT
-oc expose svc/fabric-camel-rest --hostname=fabric-camel-rest-my-project.192.168.64.3.nip.io
-oc env dc/fabric-camel-rest --from=configmap/filebeat-url
+oc create configmap camel-rest-props --from-file=./config.json
+oc new-app svsvenu/camel-rest:1.0-SNAPSHOT
+oc expose svc/camel-rest --hostname=camel-rest-my-project.192.168.64.3.nip.io
+oc set env dc/camel-rest --from=configmap/camel-rest-props
 oc new-app sebp/elk
 oc expose svc/elk --port=5601 --hostname=fabric-elk-rest-my-project.192.168.64.3.nip.io
 oc expose svc/elk --name=elk-elastic --port=9200 --hostname=fabric-elastic-rest-my-project.192.168.64.3.nip.io
